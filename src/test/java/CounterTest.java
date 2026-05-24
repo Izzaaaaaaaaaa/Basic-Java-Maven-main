27859b36972e9fe41d64173ce39bbdc23ba37a56
izzaaaaaaaaaa <izzatyzahara2424@gmail.com>
2026-05-24 22:54:41 +0700

    Refactor test methods in CounterTest class for improved readability and consistency

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -1,53 +1,51 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-
-
 public class CounterTest {
 
-	@Test
-	public void testReset() {
-		Counter testCounter = new Counter();
-		
-		for(int i = 0; i <10; i++) {
-			testCounter.increment();
-		}
-		
-		testCounter.reset();
-		
-		assertEquals(testCounter.getCount(), 0);
-	}
-
-	@Test
-	public void testIncrement() {
-    Counter testCounter = new Counter();
-    
-    for(int i = 1; i <10; i++) {
-        testCounter.increment();
-
-        // ERROR LOGIKA
-        assertEquals(testCounter.getCount(), i + 1);
+    @Test
+    public void testReset() {
+        Counter testCounter = new Counter();
+        
+        for(int i = 0; i < 10; i++) {
+            testCounter.increment();
+        }
+        
+        testCounter.reset();
+        
+        assertEquals(testCounter.getCount(), 0);
     }
-}
 
-	// @Test
-	// public void testIncrement() {
-	// 	Counter testCounter = new Counter();
-		
-	// 	for(int i = 1; i <10; i++) {
-	// 		testCounter.increment();
-	// 		assertEquals(testCounter.getCount(), i);
-	// 	}
-	// }
+    // Mengubah nama agar tidak error compile, tapi isinya tetap duplikat
+    @Test
+    public void testIncrementWithLogicError() {
+        Counter testCounter = new Counter();
+        
+        for(int i = 1; i < 10; i++) {
+            testCounter.increment();
 
-	@Test
-	public void testDecrement() {
-		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
-			testCounter.decrement();
-			assertEquals(testCounter.getCount(), i * -1);
-		}
-	}
+            // ERROR LOGIKA (Duplikasi struktural yang tinggi)
+            assertEquals(testCounter.getCount(), i + 1);
+        }
+    }
 
+    @Test
+    public void testIncrementNormal() {
+        Counter testCounter = new Counter();
+        
+        for(int i = 1; i < 10; i++) {
+            testCounter.increment();
+            assertEquals(testCounter.getCount(), i);
+        }
+    }
+
+    @Test
+    public void testDecrement() {
+        Counter testCounter = new Counter();
+        
+        for(int i = 1; i < 10; i++) {
+            testCounter.decrement();
+            assertEquals(testCounter.getCount(), i * -1);
+        }
+    }
 }
