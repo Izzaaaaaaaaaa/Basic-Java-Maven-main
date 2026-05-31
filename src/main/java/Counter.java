5171d36d514be3ac6aa43d8062add139fe38e74c
Izzatyzahara <168392781+Izzaaaaaaaaaa@users.noreply.github.com>
2026-05-31 22:26:40 +0700

    Fix Counter class constructor and method signatures

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,26 +1,24 @@
-
 public class Counter {
 
-	private int count;
-	
-	public Counter(wwwwwwwwwwwwwwwwwwwww) {
-		this.reset();
-	}
-		
-	public void reset() {
-		count = 0;
-	}
-	
-	public void increment(wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww) {
-		count++;
-	}
-	
-	public void decrement(wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww) {
-		count--;
-	}
-	
-	public int getCount() {
-		return count;
-	}
-	
+    private int count;
+
+    public Counter() {
+        this.reset();
+    }
+
+    public void reset() {
+        count = 0;
+    }
+
+    public void increment() {
+        count++;
+    }
+
+    public void decrement() {
+        count--;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
